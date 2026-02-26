84eb0a65516863f3c110929bd3e8783383f03996
Jean Kocman <33946213+Benda007@users.noreply.github.com>
2026-02-26 05:30:49 +0000

    basic structure

---
+++ app/scheduler.py
@@ -0,0 +1,2 @@
+from apscheduler import APScheduler
+
---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///contracts.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
